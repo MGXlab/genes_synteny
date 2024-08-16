65ccc7290edbbf7a8d6530a1c4e52d943714101a
Maria Beatriz Walter Costa <waltercostamb@gmail.com>
2024-08-16 13:29:33 +0200

    Update blast.sh

--- scripts/blast.sh
+++ scripts/blast.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-#Loop over each .faa file (prodigal's gene/protein predictions) 
-for file in ../genomes/*.faa; do
+#Loop over each protein file (prodigal's gene/protein predictions) 
+for file in proteins/*.fasta; do
 
-  #Extract the base name of the file (remove the .faa extension)
-  base_name=$(basename "$file" .faa)
+  #Extract the base name of the file (remove the extension)
+  base_name=$(basename "$file" .fasta)
   
   echo "$base_name"
 
   #Create BLAST database of Prodigal's annotated proteins
-  /home/groups/VEO/tools/blast/v2.14.0/bin/makeblastdb -in ../genomes/${base_name}.faa -dbtype prot -out ../genomes/${base_name}
+  /home/groups/VEO/tools/blast/v2.14.0/bin/makeblastdb -in proteins/${base_name}.fasta -dbtype prot -out proteins/${base_name}
 
-  #BLAST Alvaro's proteins against Prodigal's annotations
-  #/home/groups/VEO/tools/blast/v2.14.0/bin/tblastn -query ../genomes/${base_name}.fna -db ../genomes/${base_name} -out ../genomes/${base_name}.out
-  /home/groups/VEO/tools/blast/v2.14.0/bin/blastp -query ../proteins/${base_name}.fasta -db ../genomes/${base_name} -outfmt 6 -out ../genomes/${base_name}.blastout
+  #BLAST proteins of interest against Prodigal's annotations
+  /home/groups/VEO/tools/blast/v2.14.0/bin/blastp -query proteins/${base_name}_proteins_of_interest.fasta -db proteins/${base_name} -outfmt 6 -out proteins/${base_name}.blastout
 done
-
