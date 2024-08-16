35cb1234dce03fd1c464b42ef679d5e159d889cf
Maria Beatriz Walter Costa <waltercostamb@gmail.com>
2024-08-16 15:07:59 +0200

    Update barrnap.sh

--- scripts/barrnap.sh
+++ scripts/barrnap.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+#Activate environment
+source /vast/groups/VEO/tools/anaconda3/etc/profile.d/conda.sh
+conda activate barrnap_v0.9
+
 #Loop over each .fna file (genome) 
 for file in genomes/*.fna; do
 
   #Extract the base name of the file (remove the .fna extension)
   base_name=$(basename "$file" .fna)
-  
-  #echo "$base_name"
 
+  #echo "$base_name"
+  
   #Get 16S genes from barrnap
   barrnap genomes/${base_name}.fna > 16S_genes/${base_name}.gff
 
   #Get Fasta sequences
-  bedtools getfasta -s -fo 16S_genes/${base_name}.fasta -fi genomes/${base_name}.fna -bed 16S_genes/${base_name}.gff
+  /home/groups/VEO/tools/bedtools2/v2.31.0/bin/bedtools getfasta -s -fo 16S_genes/${base_name}.fasta -fi genomes/${base_name}.fna -bed 16S_genes/${base_name}.gff
 
   #Copy all sequences to file, that will be processed manually
   cp 16S_genes/${base_name}.fasta 16S_genes/${base_name}_16S.fasta
