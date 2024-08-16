eee027d7a6ca05d88f5192ac7697dd87e2ba483b
Maria Beatriz Walter Costa <waltercostamb@gmail.com>
2024-08-16 15:04:32 +0200

    Update barrnap.sh

--- scripts/barrnap.sh
+++ scripts/barrnap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Loop over each .fna file (genome) 
-for file in ../genomes/*.fna; do
+for file in genomes/*.fna; do
 
   #Extract the base name of the file (remove the .fna extension)
   base_name=$(basename "$file" .fna)
@@ -9,13 +9,12 @@ for file in ../genomes/*.fna; do
   #echo "$base_name"
 
   #Get 16S genes from barrnap
-  barrnap ../genomes/${base_name}.fna > ../16S_genes/${base_name}.gff
+  barrnap genomes/${base_name}.fna > 16S_genes/${base_name}.gff
 
   #Get Fasta sequences
-  bedtools getfasta -s -fo ../16S_genes/${base_name}.fasta -fi ../genomes/${base_name}.fna -bed ../16S_genes/${base_name}.gff
+  bedtools getfasta -s -fo 16S_genes/${base_name}.fasta -fi genomes/${base_name}.fna -bed 16S_genes/${base_name}.gff
 
   #Copy all sequences to file, that will be processed manually
-  cp ../16S_genes/${base_name}.fasta ../16S_genes/${base_name}_16S.fasta
+  cp 16S_genes/${base_name}.fasta 16S_genes/${base_name}_16S.fasta
 
 done
-
